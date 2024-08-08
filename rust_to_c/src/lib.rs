990869e81b691b7a1b10007dd1c860e29501b2ae
perturbing <thomas.vellekoop@iohk.io>
2024-08-08 13:13:20 +0200

    change function again

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -3,8 +3,13 @@ use ff::Field;
 use rand::thread_rng;
 
 #[no_mangle]
-pub extern "C" fn random_scalar(a: Scalar) -> Scalar {
-    println!("a: {:?}", a);
+pub extern "C" fn random_scalar(a: *const Scalar) -> Scalar {
+    if a.is_null() {
+        println!("Received null pointer");
+        return Scalar::ZERO;
+    }
+    let a_scalar = unsafe { *a };
+    println!("a: {:?}", a_scalar);
     let mut rng = thread_rng();
     let b = Scalar::random(&mut rng);
     println!("b: {:?}", b);
