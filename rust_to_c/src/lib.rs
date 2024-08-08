d369a58f905a138127c035ce927ada035b56f3ca
perturbing <thomas.vellekoop@iohk.io>
2024-08-08 12:40:44 +0200

    change function again

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -3,7 +3,10 @@ use ff::Field;
 use rand::thread_rng;
 
 #[no_mangle]
-pub extern "C" fn double(a: Scalar) -> Scalar {
+pub extern "C" fn random_scalar(a: Scalar) -> Scalar {
+    println!("a: {:?}", a);
     let mut rng = thread_rng();
-    return Scalar::random(&mut rng);
+    let b = Scalar::random(&mut rng);
+    println!("b: {:?}", b);
+    return b;
 }
