a1bd836ff324ad6f2b9126ba8bd58d82033f0bae
perturbing <thomas.vellekoop@iohk.io>
2024-08-09 11:49:41 +0200

    change function again

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -8,13 +8,7 @@ pub extern "C" fn random_scalar(a: *mut Scalar) -> Scalar {
 
     if a.is_null() {
         println!("Received null pointer!");
-        return Scalar::ZERO; 
-    } else {
-        unsafe {
-            let scalar_value = *a;
-            println!("Received scalar: {:?}", scalar_value);
-        }
-    }
+    } 
 
     let mut rng = thread_rng();
     let b = Scalar::random(&mut rng);
