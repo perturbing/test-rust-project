b1c040c5c0ea0ae8bb5b499d32244ee0973e6a8e
perturbing <thomas.vellekoop@iohk.io>
2024-08-13 12:53:08 +0200

    set function again to fail if input null

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -3,16 +3,17 @@ use ff::Field;
 use rand::thread_rng;
 
 #[no_mangle]
-pub extern "C" fn random_scalar(a: *mut u8) -> Scalar {
-    // println!("Pointer address received in Rust: {:p}", a);
+pub extern "C" fn random_scalar(a: *mut Scalar) -> Scalar {
+    println!("Pointer address received in Rust: {:p}", a);
 
-    let scalar: &mut Scalar = unsafe { &mut *(a as *mut Scalar) };
-    println!("a : {:?}", scalar);
+    if a.is_null() {
+        println!("Received null pointer!");
+    } else {
+        println!("input is: {:?}", a);
+    }
 
     let mut rng = thread_rng();
-    *scalar = Scalar::random(&mut rng);
-    
-    println!("Generated random scalar: {:?}", *scalar);
-    
-    *scalar
+    let b = Scalar::random(&mut rng);
+    println!("Generated random scalar: {:?}", b);
+    return b;
 }
