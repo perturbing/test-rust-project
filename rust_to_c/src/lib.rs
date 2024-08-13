99d768d113294f15de91253426e16b742f6a3e3f
perturbing <thomas.vellekoop@iohk.io>
2024-08-13 16:02:08 +0200

    test

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -6,24 +6,16 @@ type ScalarPtr = *mut Scalar;
 
 #[no_mangle]
 pub extern "C" fn random_scalar(a: *mut ScalarPtr) -> Scalar {
-    unsafe {
-        if let Some(a_ref) = a.as_mut() {
-            if !(*a_ref).is_null() {
-                println!("Pointer is valid, proceeding with random scalar generation.");
-                println!("Current value of a: {:?}", **a_ref);
-            } else {
-                println!("Pointer is valid, but the pointed-to Scalar is null.");
-            }
+    println!("Pointer address received in Rust: {:p}", a);
 
-            let mut rng = thread_rng();
-            let b = Scalar::random(&mut rng);
-            println!("Generated random scalar: {:?}", b);
-
-            *a_ref = Box::into_raw(Box::new(b));
-            b // Return the generated Scalar
-        } else {
-            println!("Received null pointer!");
-            Scalar::ZERO // Return a default zero Scalar in case of error
-        }
+    if a.is_null() {
+        println!("Received null pointer!");
+    } else {
+        println!("input is: {:?}", a);
     }
+
+    let mut rng = thread_rng();
+    let b = Scalar::random(&mut rng);
+    println!("Generated random scalar: {:?}", b);
+    return b;
 }
