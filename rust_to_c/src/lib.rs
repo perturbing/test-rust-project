e335d130e9f786bfb9b9311a6405a47d39c35c83
perturbing <thomas.vellekoop@iohk.io>
2024-08-13 16:00:27 +0200

    try wrappingng the scalar

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -2,18 +2,28 @@ use blstrs::Scalar;
 use ff::Field;
 use rand::thread_rng;
 
+type ScalarPtr = *mut Scalar;
+
 #[no_mangle]
-pub extern "C" fn random_scalar(a: *mut Scalar) -> Scalar {
-    println!("Pointer address received in Rust: {:p}", a);
+pub extern "C" fn random_scalar(a: *mut ScalarPtr) -> Scalar {
+    unsafe {
+        if let Some(a_ref) = a.as_mut() {
+            if !(*a_ref).is_null() {
+                println!("Pointer is valid, proceeding with random scalar generation.");
+                println!("Current value of a: {:?}", **a_ref);
+            } else {
+                println!("Pointer is valid, but the pointed-to Scalar is null.");
+            }
 
-    if a.is_null() {
-        println!("Received null pointer!");
-    } else {
-        println!("input is: {:?}", a);
+            let mut rng = thread_rng();
+            let b = Scalar::random(&mut rng);
+            println!("Generated random scalar: {:?}", b);
+
+            *a_ref = Box::into_raw(Box::new(b));
+            b // Return the generated Scalar
+        } else {
+            println!("Received null pointer!");
+            Scalar::ZERO // Return a default zero Scalar in case of error
+        }
     }
-
-    let mut rng = thread_rng();
-    let b = Scalar::random(&mut rng);
-    println!("Generated random scalar: {:?}", b);
-    return b;
 }
