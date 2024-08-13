5906223dff3b6171c3933e6a0d66f2dd00084c15
perturbing <thomas.vellekoop@iohk.io>
2024-08-13 12:51:21 +0200

    test with casting pointer to scalar in function

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -3,15 +3,16 @@ use ff::Field;
 use rand::thread_rng;
 
 #[no_mangle]
-pub extern "C" fn random_scalar(a: *mut Scalar) -> Scalar {
-    println!("Pointer address received in Rust: {:p}", a);
+pub extern "C" fn random_scalar(a: *mut u8) -> Scalar {
+    // println!("Pointer address received in Rust: {:p}", a);
 
-    if a.is_null() {
-        println!("Received null pointer!");
-    } 
+    let scalar: &mut Scalar = unsafe { &mut *(a as *mut Scalar) };
+    println!("a : {:?}", scalar);
 
     let mut rng = thread_rng();
-    let b = Scalar::random(&mut rng);
-    println!("Generated random scalar: {:?}", b);
-    return b;
+    *scalar = Scalar::random(&mut rng);
+    
+    println!("Generated random scalar: {:?}", *scalar);
+    
+    *scalar
 }
