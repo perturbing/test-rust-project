bc2f44f962b5b5eb4ad208a4a024c6846212c9cd
perturbing <thomas.vellekoop@iohk.io>
2024-08-26 12:39:43 +0200

    do not mutate input of list scalars

--- rust_to_c/src/lib.rs
+++ rust_to_c/src/lib.rs
@@ -9,18 +9,19 @@ pub extern "C" fn random_scalar_list(scalars_ptr: *mut Scalar, len: usize) {
 
     if !scalars_ptr.is_null() {
         // Convert the raw pointer to a mutable slice
+        println!("Received length: {}", len);
         let scalars: &mut [Scalar] = unsafe { slice::from_raw_parts_mut(scalars_ptr, len) };
         for scalar in scalars.iter_mut() {
             println!("Received scalar: {:?}", scalar);
         }
         
-        let mut rng = thread_rng();
+        // let mut rng = thread_rng();
 
         // Modify each scalar in the slice
-        for scalar in scalars.iter_mut() {
-            *scalar = Scalar::random(&mut rng);
-            println!("Modified scalar: {:?}", scalar);
-        }
+        // for scalar in scalars.iter_mut() {
+            // *scalar = Scalar::random(&mut rng);
+            // println!("Modified scalar: {:?}", scalar);
+        // }
     } else {
         println!("Received null pointer!");
     }
